c4995b99ddca9e5986994bbca931e39fa2beee36
Jessica Gladstone <jcg321@comcast.net>
2019-02-04 16:22:40 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,7 +12,13 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
-      
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+    else
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+        end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
